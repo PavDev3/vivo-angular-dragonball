a16b3cfb14cddff3c3ad6f14079b1d31506a1e08
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-26 11:09:43 -0500

    Implement character loading functionality and update characters page layout

--- src/app/dragonball/pages/characters-page/characters-page.component.ts
+++ src/app/dragonball/pages/characters-page/characters-page.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { DragonballService } from '../../services/dragonball.service';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'app-characters-page',
   imports: [],
   templateUrl: './characters-page.component.html',
 })
-export class CharactersPageComponent {}
+export class CharactersPageComponent implements OnInit {
+  dragonballService = inject(DragonballService);
+
+  characters = signal<Character[]>([]);
+
+  ngOnInit(): void {
+    this.dragonballService.loadCharacters().subscribe((characters) => {
+      this.characters.set(characters);
+    });
+  }
+}
--- src/app/dragonball/services/dragonball.service.ts
+++ src/app/dragonball/services/dragonball.service.ts
@@ -1,13 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
+import { map, Observable } from 'rxjs';
+
+import type {
+  Character,
+  CharacterResponse,
+} from '../interfaces/character.interface';
+import { environment } from '@envs/environment.development';
 
 @Injectable({ providedIn: 'root' })
-export class ServiceNameService {
+export class DragonballService {
   private http = inject(HttpClient);
   private api = environment.dragonballApi;
 
-  loadCharacters(page: number = 1) {
-    return this.http.get(`${this.api}/character?page=${page}`);
+  loadCharacters(page: number = 1): Observable<Character[]> {
+    return this.http
+      .get<CharacterResponse>(`${this.api}/character?page=${page}`)
+      .pipe(map((response) => response.items));
   }
 }
