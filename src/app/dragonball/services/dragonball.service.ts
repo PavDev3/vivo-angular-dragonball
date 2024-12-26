34b60195f483bd2c647e383707306e1811e69538
Fernando Herrera <fernando.herrera85@gmail.com>
2024-12-26 12:46:39 -0500

    Add navigation for previous character and implement caching in character service

--- src/app/dragonball/services/dragonball.service.ts
+++ src/app/dragonball/services/dragonball.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { catchError, delay, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 
 import type {
   Character,
@@ -14,22 +14,29 @@ export class DragonballService {
   private http = inject(HttpClient);
   private api = environment.dragonballApi;
 
+  private characterCache = new Map<string, FullCharacter>();
+
   loadCharacters(page: number = 1): Observable<Character[]> {
     return this.http
       .get<CharacterResponse>(`${this.api}/characters?page=${page}`)
       .pipe(
-        map((response) => response.items)
-        // delay(2500)
+        map((response) => response.items),
+        delay(2500)
       );
   }
 
   loadCharacter(id: string): Observable<FullCharacter> {
+    if (this.characterCache.has(id)) {
+      return of(this.characterCache.get(id)!);
+    }
+
     return this.http.get<FullCharacter>(`${this.api}/characters/${id}`).pipe(
+      delay(2500),
+      tap((character) => this.characterCache.set(id, character)),
       catchError((error) => {
         console.error('Error:', error);
         return throwError(() => `Character by id ${id} not found`);
       })
-      // delay(2500)
     );
   }
 }
