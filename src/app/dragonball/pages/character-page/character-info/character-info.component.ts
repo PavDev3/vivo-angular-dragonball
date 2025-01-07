1ba58896ea89fa01ae0be219e7e0246cfcad8b01
pablo <pnunfe@gmail.com>
2025-01-07 14:40:56 +0100

    Remove imports doesn't use

--- src/app/dragonball/pages/character-page/character-info/character-info.component.ts
+++ src/app/dragonball/pages/character-page/character-info/character-info.component.ts
@@ -1,13 +1,8 @@
 import { Component, input } from '@angular/core';
-import { RouterLink } from '@angular/router';
-import {
-  Character,
-  FullCharacter,
-} from 'src/app/dragonball/interfaces/character.interface';
+import { FullCharacter } from 'src/app/dragonball/interfaces/character.interface';
 
 @Component({
   selector: 'app-character-info',
-  imports: [RouterLink],
   templateUrl: './character-info.component.html',
 })
 export class CharacterInfoComponent {
